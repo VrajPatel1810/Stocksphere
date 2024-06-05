7d1afd9454fd72b2c406ae9503422f3ef7cf0506
Vraj Patel <202101060@daiict.ac.in>
2024-06-05 11:54:29 +0530

    Frontend Folder Initialized

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './pages/home';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
